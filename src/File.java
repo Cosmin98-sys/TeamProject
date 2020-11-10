580e377a56d992e85b8d22348ff693582cdb7a50
alexandra-oras-selab <larisa.oras@mta.ro>
2020-11-10 19:21:25 +0200

    Adaugare functie afisare informatii File
    Am creat functia showFile care afiseaza numele, locatia si dimensiunea fisierului
    
    Signed-off-by: alexandra-oras-selab <larisa.oras@mta.ro>

--- src/File.java
+++ src/File.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class File {
+public class File {
+    private String name;
+    private String location;
+    private int size;
+
+    public File(String name, String location, int size) {
+        this.name = name;
+        this.location = location;
+        this.size = size;
+    }
+
+    public void showFile(File f){
+        System.out.println("Fisierul %s se afla in locatia %d si are dimensiunea de %d kB", f.name, f.location, f.size);
+    }
 }
